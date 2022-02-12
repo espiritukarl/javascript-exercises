f338f84fe45c61b596530f57571b53ee6fc5033d
Karl Espiritu <karlespiritukarl@gmail.com>
2022-02-12 22:30:35 +0800

    Added solution to Java Exercises 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (testArray, ...args) {
+    let array = Array.from(args);
 
-};
+    for (element of array) {
+        if (testArray.includes(element) != 0) {
+            testArray = testArray.filter(r => r != element)
+        }
+    }
+
+    return testArray;
+
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
