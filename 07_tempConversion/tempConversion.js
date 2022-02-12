304b58192c712321dde150c0ff221b407cdc9ed9
Karl Espiritu <karlespiritukarl@gmail.com>
2022-02-12 23:03:14 +0800

    Added solution to JavaScript Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function (tempFarenheit) {
 
+  let result = (5 / 9) * (tempFarenheit - 32);
+
+  return Math.round(result * 10) / 10;
 };
 
-const ctof = function() {
+const ctof = function (tempCelsius) {
 
+  let result = (9 / 5) * tempCelsius + 32;
+
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
