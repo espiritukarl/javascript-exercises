164e5bc9e675d177c8d08cf651f1c43db67fd4ef
Karl Espiritu <karlespiritukarl@gmail.com>
2022-02-15 18:41:11 +0800

    Added solution to JavaScript Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
 
-};
+    for (index of people) {
+        if (!("yearOfDeath" in index)) {
+            index.yearOfDeath = new Date().getFullYear();
+        }
+    }
+
+    const yearsAlive = people.sort((a, b) =>
+        (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1
+    );
+
+    return yearsAlive[0]
+}
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
