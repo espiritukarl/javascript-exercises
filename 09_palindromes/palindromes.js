54520cd69ab15ced823f52d09886f0a633e47399
Karl Espiritu <karlespiritukarl@gmail.com>
2022-02-15 17:42:13 +0800

    Added solution to JavaScript Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g;
+    const unpunctuated = word.replace(regex, '');
 
+    const reverseString = function (str) {
+        let strReversed = '';
+        for (let i = str.length - 1; i >= 0; i--) {
+            strReversed += str[i];
+        }
+        return strReversed;
+    };
+
+    return reverseString(unpunctuated) == unpunctuated;
 };
 
 // Do not edit below this line
