e23eb81869783a676a5ac7fab6dbca7c448f797d
Karl Espiritu <karlespiritukarl@gmail.com>
2022-02-15 17:53:16 +0800

    Added solution to JavaScript Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+    if (number > 0) {
+        number--;
+        return (process(number));
+    } else return "OOPS";
 
 };
+
+let process = (number) => {
+    if (number == 0) {
+        return 1;
+    } else if (number == 1) {
+        return 1;
+    } else {
+        return process(number - 1) + process(number - 2);
+    }
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
