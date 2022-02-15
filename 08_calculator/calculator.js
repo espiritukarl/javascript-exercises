6cd5519abf7d83cf13c5dcc44dcf850d61d9b383
Karl Espiritu <karlespiritukarl@gmail.com>
2022-02-15 17:24:09 +0800

    Added solutions to JavaScript Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (a) {
+  let result = 0;
+  for (addends of a) {
+    result += addends;
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (a) {
+  let result = 1;
+  for (addends of a) {
+    result *= addends;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a == 0) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let i = 1; i <= a; i++) {
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
